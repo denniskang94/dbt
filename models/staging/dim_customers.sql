9168a392e88afaef50bf0d32d859e62247df7361
denniskang94 <jeehun.d.kang@gmail.com>
2025-07-24 08:54:50 -0400

    Staging test (#2)
    * first model
    
    * 3rd iteration

---
+++ models/staging/dim_customers.sql
@@ -0,0 +1,36 @@
+{{
+
+    config(
+        materialized = 'table'
+    )
+}}
+
+with customers as (
+    select * 
+    from {{ref('stg_customers')}}
+)
+
+, orders as(
+    select * 
+    from {{ ref('stg_orders')}})
+
+
+, customer_orders as(
+     select 
+        customer_id,
+        min(order_date) as first_order_date,
+        max(order_date) as most_recent_order_date,
+        count(order_id) as number_of_orders
+    
+    from orders
+    group by 1)
+
+    select
+        a.CUSTOMER_ID,
+        a.first_name,
+        a.last_name,
+        b.first_order_date,
+        b.most_recent_order_date,
+        coalesce(b.number_of_orders, 0) as number_of_orders
+    from customers a
+    left join customer_orders b on a.customer_id = b.customer_id
